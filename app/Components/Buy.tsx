0d4bc9fd8aa975f003639f45675b4ad40c63371c
Zion Eloheeka <m.eloheeka12@gmail.com>
2026-02-01 10:02:30 +0200

    Added the Buy Section

---
+++ app/Components/Buy.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Buy() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-[300px] '>
+        <h1 className='text-center text-white text-4xl font-bold mb-8'>Your directory idea can go live faster</h1>
+        <p className='text-gray-500 text-lg font-semibold mb-12'>QuickDir gives you the foundation. You bring the vision</p>
+        <button className='border border-blue-700 rounded-full p-5 bg-blue-700 text-white cursor-pointer'>Buy QuickDir</button>
+    </div>
+  )
+}
